3e2a554e16a5311e6ad9578a340534c940d663a4
dtwelch <dtw.welch@gmail.com>
2024-10-08 19:21:03 -0400

    more

--- src/edu/psu/ist/Row.java
+++ src/edu/psu/ist/Row.java
@@ -1,14 +1,27 @@
 package edu.psu.ist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public record Row(int rowNum, ArrayList<TileType> columns) {
 
     /** Static factory method for constructing a row. */
-    public static Row of(int rowNum, TileType ... tiles) {
-        for (var t : tiles) {
-            
+    public static Row of(int rowNum, TileType... tiles) {
+        return new Row(rowNum, new ArrayList<>(Arrays.asList(tiles)));
+    }
+
+    @Override public String toString() {
+        var first = true;
+        var sb = new StringBuilder();
+        for (var tile : columns) {
+
+            if (first) {
+                sb.append(tile.cellAsString());
+                first = false;
+            } else {
+                sb.append(" ").append(tile.cellAsString());
+            }
         }
-        return new Row(rowNum, );
+        return sb.toString();
     }
 }
